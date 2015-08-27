69c2957de9f0363d4fc153973736b9c2b482429b
Kang Peng <kangpeng@MA-DIGXP.local>
2015-08-27 13:44:29 -0400

    Updated appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,10 +1,10 @@
 const config = {
-	appTitle: 'NYPL | React Header',
-	appName: 'NYPL DGX React Header',
+	appTitle: 'NYPL | Booklists',
+	appName: 'DGX Booklists',
 	port: 3001,
 	webpackDevServerPort: 3000,
 	favIconPath: 'http://ux-static.nypl.org.s3-website-us-east-1.amazonaws.com/images/favicon.ico',
-	apiUrl: 'http://dev.refinery.aws.nypl.org/api/nypl/ndo/v0.1/site-data/header-items?filter%5Brelationships%5D%5Bparent%5D=null&include=children,related-mega-menu-panes.current-mega-menu-items.images'
+	apiUrl: 'http://dev.refinery.aws.nypl.org/api/nypl/ndo/v0.1/book-lists/book-list-users'
 };
 
 export default config;
