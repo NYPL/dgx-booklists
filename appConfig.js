5e8b7f6ef965d23f46c0f0b982ca7821b0426809
Edwin Guzman <edwin.gzmn@gmail.com>
2015-09-09 11:53:31 -0400

    Reverting back to dev refinery.

--- appConfig.js
+++ appConfig.js
@@ -4,7 +4,7 @@ const config = {
 	port: 3001,
 	webpackDevServerPort: 3000,
 	favIconPath: 'http://ux-static.nypl.org.s3-website-us-east-1.amazonaws.com/images/favicon.ico',
-	apiUrl: 'http://qa.refinery.aws.nypl.org/api/nypl/ndo/v0.1/book-lists'
+	apiUrl: 'http://dev.refinery.aws.nypl.org/api/nypl/ndo/v0.1/book-lists'
 };
 
 export default config;
