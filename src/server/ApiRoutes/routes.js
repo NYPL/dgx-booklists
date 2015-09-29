243fba02ccabe2e9425ee6f762eaaf5c7a218855
Kang Peng <kangtingpeng@nypl.org>
2015-09-29 12:59:39 -0400

    Added comments on ApiRoutes/routes.

--- src/server/ApiRoutes/routes.js
+++ src/server/ApiRoutes/routes.js
@@ -1,25 +1,36 @@
+// Import libraries
 import express from 'express';
 import axios from 'axios';
 import parser from 'jsonapi-parserinator';
 import {api} from '../../../appConfig.js';
 
+// Set up variables for routing and its options
 let router = express.Router(),
   options = {
     includes: ['user', 'list-items.item']
   };
 
+// Have parser to take the options
 parser.setChildrenObjects(options);
 
+/**
+* BookListUsers(req, res, next)
+* The route for rendering the page of all BookListUsers/Owners.
+* It utilizes the method of promise.
+*
+* @param (HTTP methods)
+*/
 function BookListUsers(req, res, next) {
   let endpoint = `${api.root}${api.baseEndpoint}${api.bookListUserEndpoint}`;
 
+  // get data from the endpoint
   axios
     .get(endpoint)
     .then(data => {
       let returnedData = data.data,
         // parse the data
         parsed = parser.parse(returnedData);
-
+      // put the data in Store
       res.locals.data = {
         Store: {
           Data: parsed
@@ -28,6 +39,7 @@ function BookListUsers(req, res, next) {
 
       next();
     })
+    // console error messages
     .catch(error => {
       console.log('Error calling API : ' + error);
       console.log('Attempted to call : ' + options.endpoint);
@@ -37,9 +49,16 @@ function BookListUsers(req, res, next) {
         }
       };
       next();
-    }); /* end Axios call */
+    }); // end Axios call
 }
 
+/**
+* BookListUser(req, res, next)
+* The route for rendering the page of one BookListUsers/Owners.
+* It utilizes the method of promise.
+*
+* @param (HTTP methods)
+*/
 function BookListUser(req, res, next) {
   let username = req.params.username,
     endpoint = `${api.root}${api.baseEndpoint}${api.bookListUserEndpoint}/` +
@@ -57,6 +76,7 @@ function BookListUser(req, res, next) {
       };
       next();
     })
+    // console error messages
     .catch(error => {
       console.log('Error calling API : ' + error);
       console.log('Attempted to call : ' + options.endpoint);
@@ -66,9 +86,16 @@ function BookListUser(req, res, next) {
         }
       };
       next();
-    }); /* end Axios call */
+    }); // end Axios call
 }
 
+/**
+* ListID(req, res, next)
+* The route for rendering the page of Singlelist.
+* It utilizes the method of promise.
+*
+* @param (HTTP methods)
+*/
 function ListID(req, res, next) {
   let listID = req.params.listID,
     endpoint = `${api.root}${api.baseEndpoint}/${listID}${api.includes}`;
@@ -95,9 +122,16 @@ function ListID(req, res, next) {
         }
       };
       next();
-    }); /* end Axios call */
+    }); // end Axios call
 }
 
+/**
+* AjaxBookListUser(req, res, next)
+* The AJAX call for internal browsing to the page of one BookListUsers/Owners.
+* It utilizes the method of promise.
+*
+* @param (HTTP methods)
+*/
 function AjaxBookListUser(req, res, next) {
   let username = req.params.username,
     endpoint = `${api.root}${api.baseEndpoint}${api.bookListUserEndpoint}/` +
@@ -117,9 +151,16 @@ function AjaxBookListUser(req, res, next) {
     .catch(error => {
       console.log('Error calling API');
       res.json({'error': 'error calling API'});
-    }); /* end Axios call */
+    }); // end Axios call
 }
 
+/**
+* AjaxListID(req, res, next)
+* The AJAX call for internal browsing to the page of one Singlelist.
+* It utilizes the method of promise.
+*
+* @param (HTTP methods)
+*/
 function AjaxListID(req, res, next) {
   let listID = req.params.listID,
     endpoint = `${api.root}${api.baseEndpoint}/${listID}${api.includes}`;
@@ -138,9 +179,10 @@ function AjaxListID(req, res, next) {
     .catch(error => {
       console.log('Error calling API');
       res.json({'error': 'error calling API'});
-    }); /* end Axios call */
+    }); // end Axios call
 }
 
+// Express routing methods, execute the methods via URL to render the pages
 router
   .route('/')
   .get(BookListUsers);
