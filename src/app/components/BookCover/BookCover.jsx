0bd47b4fafcb338935265a7262c438ee43df480c
Kang Peng <kangtingpeng@nypl.org>
2015-10-15 11:36:25 -0400

    Set up environment for detecting 1 by 1 picture.

--- src/app/components/BookCover/BookCover.jsx
+++ src/app/components/BookCover/BookCover.jsx
@@ -6,27 +6,42 @@ class BookCover extends React.Component {
   // Constructor used in ES6
   constructor(props) {
     super(props);
+
+    this.state = {
+      imageEndpoint : 'https://contentcafe2.btol.com/ContentCafe/' +
+        'Jacket.aspx?&userID=NYPL49807&password=CC68707&' +
+        'Value=',
+      imageArgument : '&content=M&Return=1&Type=M',
+      placeHolderEndpoint : '/images/book-place-holder.png',
+      isLoaded : false
+    }
   }
+
+  // Listen to the change from data
+  // componentDidMount() {
+  //   Store.listen(this._onChange.bind(this));
+  // }
+
+  // // Stop listening
+  // componentWillUnmount() {
+  //   Store.unlisten(this._onChange.bind(this));
+  // }
 
   render () {
     let imageSrc = (this.props.isbn && this.props.isbn !== undefined) ?
-      `https://contentcafe2.btol.com/ContentCafe/` +
-      `Jacket.aspx?&userID=NYPL49807&password=CC68707&` +
-      `Value=${this.props.isbn}&content=M&Return=1&Type=M` :
+      `${this.state.imageEndpoint}${this.props.isbn}${this.state.imageArgument}` :
       // Show the place holder if the book cover's ISBN is not available
-      `/images/book-place-holder.png`,
-      img,
-      loaded = false;
+      this.state.placeHolderEndpoint,
+      imageElement;
 
       console.log(imageSrc);
 
-        img = this.refs.coverImage;
 //   img.addEventListener('load', () => {loaded = true; }, true);
 //   wait = setInterval(function () {
 //     loaded ? clearInterval(wait) : console.log(img.width, 'x', img.height);
 // }, 0);
 
-console.log(img);
+    
 
     return (
       <div id={`${this.props.id}-place-holder`}>
@@ -35,12 +50,18 @@ console.log(img);
         src={imageSrc} alt={this.props.name} />
       </div>
     );
+
+
   }
 
-
+ // this.imageElement = document.getElementById(this.props.id);
+ // console.log('>>>>>>>>>>>>>' + this.img);
 
 
 };
+
+
+
 
 BookCover.defaultProps = {
   lang: 'en'
