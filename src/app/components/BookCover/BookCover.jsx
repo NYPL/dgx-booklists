42b3832a973f1905e3f8a549a69a2e76330416b0
Kang Peng <kangtingpeng@nypl.org>
2015-10-15 12:51:06 -0400

    Set up ref to image element.

--- src/app/components/BookCover/BookCover.jsx
+++ src/app/components/BookCover/BookCover.jsx
@@ -18,28 +18,30 @@ class BookCover extends React.Component {
   }
 
   // Listen to the change from data
-  // componentDidMount() {
-  //   Store.listen(this._onChange.bind(this));
-  // }
+  componentDidMount() {
+    this.refs.coverImage.getDOMNode().addEventListener('load', () => {loaded = true; }, true);
+  }
 
-  // // Stop listening
-  // componentWillUnmount() {
-  //   Store.unlisten(this._onChange.bind(this));
-  // }
+  // Stop listening
+  componentWillUnmount() {
+    
+  }
 
   render () {
     let imageSrc = (this.props.isbn && this.props.isbn !== undefined) ?
       `${this.state.imageEndpoint}${this.props.isbn}${this.state.imageArgument}` :
       // Show the place holder if the book cover's ISBN is not available
       this.state.placeHolderEndpoint,
+      loaded = false,
+      wait = setInterval(function () {
+        loaded ? clearInterval(wait) : console.log('done!');
+      }, 0),
       imageElement;
 
-      console.log(imageSrc);
+      // console.log(imageSrc);
 
 //   img.addEventListener('load', () => {loaded = true; }, true);
-//   wait = setInterval(function () {
-//     loaded ? clearInterval(wait) : console.log(img.width, 'x', img.height);
-// }, 0);
+
 
     
 
@@ -51,12 +53,14 @@ class BookCover extends React.Component {
       </div>
     );
 
+//  imageElement = this.refs.coverImage;
+
+// console.log(imageElement);
 
   }
 
  // this.imageElement = document.getElementById(this.props.id);
  // console.log('>>>>>>>>>>>>>' + this.img);
-
 
 };
 
