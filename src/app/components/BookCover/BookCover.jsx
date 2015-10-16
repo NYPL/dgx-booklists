dfd6b57923eb6365d61e504aad6a98e12a76b4ac
Edwin Guzman <edwin.gzmn@gmail.com>
2015-10-16 15:50:53 -0400

    Updating component to call image placeholder.

--- src/app/components/BookCover/BookCover.jsx
+++ src/app/components/BookCover/BookCover.jsx
@@ -58,7 +58,7 @@ BookCover.defaultProps = {
     'Jacket.aspx?&userID=NYPL49807&password=CC68707&' +
     'Value=',
   imageArgument : '&content=M&Return=1&Type=M',
-  placeHolderEndpoint : '/images/book-place-holder.png',
+  placeHolderEndpoint : '/browse/recommendations/lists/images/book-place-holder.png',
   isbn: ''
 };
 
