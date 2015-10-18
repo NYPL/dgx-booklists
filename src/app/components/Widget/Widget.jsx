59f271f3b4487801e3460bdbd422201bda79d402
Kang Peng <kangtingpeng@nypl.org>
2015-10-18 13:26:29 -0400

    Made Widget.jsx in ES5 syntax so we can use transition.

--- src/app/components/Widget/Widget.jsx
+++ src/app/components/Widget/Widget.jsx
@@ -1,5 +1,6 @@
 // Import React Libraries
 import React from 'react';
+import Router from 'react-router';
 import Radium from 'radium';
 
 // ALT FLUX
@@ -14,75 +15,115 @@ import PaginationButton from '../Buttons/PaginationButton.jsx';
 
 import utils from '../../utils/utils.js';
 
-// Create the class. Use ES5 for react-router Navigation
-class BookItemList extends React.Component {
-  constructor(props) {
-    super(props);
+// The method allows us to transit between pages internally
+let Navigation = Router.Navigation,
 
-    this.state = Store.getState();
-  }
+  // Create the class. Use ES5 for react-router Navigation
+  BookItemList = React.createClass({
+  // For internal transition
+    mixins: [Navigation],
 
-  componentDidMount() {
-    Store.listen(this._onChange.bind(this));
-  }
+    getInitialState() {
+      return {
+        storeData: Store.getState()
+      };
+    },
 
-  componentWillUnmount() {
-    Store.unlisten(this._onChange.bind(this));
-  }
+    componentDidMount() {
+      Store.listen(this._onChange.bind(this));
+    },
 
-  _onChange() {
-    this.setState(Store.getState());
-  }
+    componentWillUnmount() {
+      Store.unlisten(this._onChange.bind(this));
+    },
 
-  render() {
-    // The variable to store the data from Store
-    let bookItemList = this.state.bookItemList,
-      userId = bookItemList.user ? bookItemList.user.id : '',
-      userDisplayName = bookItemList.user ? bookItemList.user.attributes.name : '',
-      listId = bookItemList.id || '',
-      listItems = bookItemList['list-items'] ? bookItemList['list-items'] : [],
-      listName = bookItemList.attributes ? bookItemList.attributes['list-name'] : '',
-      listIntro = bookItemList.attributes ? bookItemList.attributes['list-description'] : '',
-      encoreUrl = 'http://browse.nypl.org/iii/encore/record/C__Rb',
-      bookCoverItems = (listItems && listItems.length) ?
-        listItems.map((element, i) => {
-          let target = `${encoreUrl}${element.item.id}?lang=eng`,
-            itemId = element.item.id,
-            bookItemName = element.item.attributes.title,
-            bookCoverIsbn = element.item.attributes.isbns[0];
+    _onChange() {
+      this.setState(Store.getState());
+    },
 
-          return(
-            <li>
-              <a href={target} className='bookItem' target='_parent'>
-                <BookCover
-                  id={itemId}
-                  isbn={bookCoverIsbn}
-                  name={bookItemName}
-                  className='cover-image' key={i}/>
-              </a>
-            </li>
-          );
-        })
-        : null;
+    render() {
+      // The variable to store the data from Store
+      let bookItemList = this.state.storeData.bookItemList,
+        userId = bookItemList.user ? bookItemList.user.id : '',
+        userDisplayName = bookItemList.user ? bookItemList.user.attributes.name : '',
+        listId = bookItemList.id || '',
+        listItems = bookItemList['list-items'] ? bookItemList['list-items'] : [],
+        listName = bookItemList.attributes ? bookItemList.attributes['list-name'] : '',
+        listIntro = bookItemList.attributes ? bookItemList.attributes['list-description'] : '',
+        encoreUrl = 'http://browse.nypl.org/iii/encore/record/C__Rb',
+        bookCoverItems = (listItems && listItems.length) ?
+          listItems.map((element, i) => {
+            let target = `${encoreUrl}${element.item.id}?lang=eng`,
+              itemId = element.item.id,
+              bookItemName = element.item.attributes.title,
+              bookCoverIsbn = element.item.attributes.isbns[0];
 
-    if (bookCoverItems !== null) {
-      styles.bookItemsWidth.width = `${bookCoverItems.length * 149 - 29}px`;
-    }
+            return(
+              <li>
+                <a href={target} className='bookItem' target='_parent'>
+                  <BookCover
+                    id={itemId}
+                    isbn={bookCoverIsbn}
+                    name={bookItemName}
+                    className='cover-image' key={i}/>
+                </a>
+              </li>
+            );
+          })
+          : null;
 
-    // Render the list of owners on DOM
-    return (
-      <div>
-        <div id='widget-container' className='widget-container'>
-          <ul id={`${this.props.id}`} className={`${this.props.className}`}
-            style={styles.bookItemsWidth}>
-            {bookCoverItems}
-          </ul>
+      if (bookCoverItems !== null) {
+        styles.bookItemsWidth.width = `${bookCoverItems.length * 149 - 29}px`;
+      }
+
+      // Render the list of owners on DOM
+      return (
+        <div>
+          <div id='widget-container' className='widget-container'>
+            <ul id={`${this.props.id}`} className={`${this.props.className}`}
+              style={styles.bookItemsWidth}>
+              {bookCoverItems}
+            </ul>
+          </div>
+          <a className={`${this.props.className}-listLink`} onClick={this._fetchBookData}>
+            <p className={`${this.props.className}-listTitle`}>{listName} @ {userDisplayName}</p>
+          </a>
         </div>
-        <p className={`${this.props.className}-listTitle`}>{listName} @ {userDisplayName}</p>
-      </div>
-    );
-  }
-}
+      );
+    },
+
+    /**
+    * _fetchBookData(e)
+    * Make an AJAX call to get the data, and then transit to the route.
+    *
+    * @param {event} e
+    */
+    _fetchBookData(e) {
+      e.preventDefault();
+      $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: `/browse/recommendations/lists/api/ajax/listID/${this.props.listId}`,
+        error: (jqXHR, textStatus, errorThrown) => {
+          console.log(`Unavailabe to complete the request. Run into a ${textStatus} for ${errorThrown}`);
+        },
+        success: data => {
+          // Update the Store for a specific list of books:
+          Actions.updateBookList(data.data);
+          // Transition to the new route.
+          this._transitionTo(this.props.userId, this.props.listId);
+        }
+      });
+    },
+
+    _transitionTo(userId, listId) {
+      // Transition to the new route.
+      this.transitionTo('BookItemList', {
+        UserLists: userId,
+        id: listId
+      });
+    }
+  });
 
 let styles = {
   bookItemsWidth: {
