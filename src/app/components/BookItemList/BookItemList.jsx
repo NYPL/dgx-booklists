8c03ce48e6d64c4146a7e1ba3a659e491fc80396
Patricio Gabriel Maseda <pmaseda@devspark.com>
2016-06-24 17:05:14 -0300

    WEB-3039-use-axios files eslinted.

--- src/app/components/BookItemList/BookItemList.jsx
+++ src/app/components/BookItemList/BookItemList.jsx
@@ -1,9 +1,6 @@
 // Import React Libraries
 import React from 'react';
 
-// Import Router and it's navigation
-import Router from 'react-router';
-
 import DocMeta from 'react-doc-meta';
 import axios from 'axios';
 
@@ -13,8 +10,6 @@ import Actions from '../../actions/Actions.js';
 
 // Import Components
 import Hero from '../Hero/Hero';
-import SimpleButton from '../Buttons/SimpleButton';
-import BookCover from '../BookCover/BookCover';
 import BookItem from '../BookItem/BookItem';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import BackButton from '../BackButton/BackButton';
@@ -22,120 +17,28 @@ import BackButton from '../BackButton/BackButton';
 import utils from '../../utils/utils.js';
 
 // Create the class. Use ES5 for react-router Navigation
-let BookItemList = React.createClass({
+const BookItemList = React.createClass({
   getInitialState() {
     return Store.getState();
   },
   // Listen to the change from data
   componentDidMount() {
-    //Store.listen(this._onChange.bind(this));
+    // Store.listen(this._onChange.bind(this));
     // As a fallback, we check if the app fails to fetch the data.
     // If so, the app will attempt to make a call on client-side one more time.
     // Also, if the users browse the app with backward button or forward button,
     // we need to check if they have refreshed the page and lost the data in the Store.
-    if(!this.state.bookItemList) {
+    if (!this.state.bookItemList) {
       this._fetchBookData();
     }
   },
   // Stop listening
   componentWillUnmount() {
-    //Store.unlisten(this._onChange.bind(this));
+    // Store.unlisten(this._onChange.bind(this));
   },
   // Change the this.state here if find any different
   _onChange() {
     this.setState(Store.getState());
-  },
-  // Render DOM
-  render() {
-    // The variable to store the data from Store
-    let bookItemList = this.state.bookItemList ? this.state.bookItemList : [],
-      userId = bookItemList.user ? bookItemList.user.id : '',
-      userDisplayName = bookItemList.user ? bookItemList.user.attributes.name : '',
-      listId = bookItemList.id || '',
-      listItems = bookItemList['list-items'] ? bookItemList['list-items'] : [],
-      listName = bookItemList.attributes ? bookItemList.attributes['list-name'] : '',
-      listIntro = bookItemList.attributes ? bookItemList.attributes['list-description'] : '',
-      encoreUrl = 'http://browse.nypl.org/iii/encore/record/C__Rb',
-      description = listIntro || 'A list created by staff at The New York Public Library',
-      pageTags = [
-        // Required OG meta tags
-        {property: "og:title", content: `${listName} | The New York Public Library`},
-        {property: "og:url", content: `http://www.nypl.org/browse/recommendations/lists/${userId}/${listId}`},
-        {property: "og:image", content: ''},
-        {property: "og:description", content: description},
-        {name: "twitter:title", content: `${listName} | The New York Public Library`},
-        {name: "twitter:description", content: description},
-        {name: "twitter:image", content: ''}
-      ],
-      tags = utils.metaTagUnion(pageTags),
-      bookItems = (listItems && listItems.length) ?
-        listItems.map((element, i) => {
-          let target = `${encoreUrl}${element.item.id}?lang=eng`,
-            itemId = element.item.id,
-            bookItemName = element.item.attributes.title,
-            bookItemDescription = element.attributes.annotation,
-            bookCoverIsbn = element.item.attributes.isbns[0],
-            authors = (element.item.attributes.authors.length) ?
-              `By ${element.item.attributes.authors}` : `The author of this item is not available`,
-            publishedDate = (element.item.attributes['publication-date'] && !undefined) ?
-              element.item.attributes['publication-date'] : 'publish date is not available',
-            catalogInfo = `${element.item.attributes.format} - ${publishedDate}`;
-
-          return(
-            <BookItem id={`bookItem`}
-              className={`bookItem`}
-              key={i}
-              itemId={itemId}
-              target={target}
-              catalogInfo={catalogInfo}
-              bookItemName={bookItemName}
-              bookItemDescription={bookItemDescription}
-              bookCoverIsbn={bookCoverIsbn}
-              authors={authors} />
-          );
-        })
-        : <ErrorMessage errorClass='book-item-list-error no-data'
-            messageContent={this.props.errorMessage.noData} />,
-      content = <div className='bookItemList-wrapper'>
-                  <BackButton
-                    userId={userId}
-                    userDisplayName={userDisplayName}
-                    />
-                  <div id={this.props.id} className={this.props.className}>
-                    <div id={`${this.props.id}__${listName}`}
-                    className={`${this.props.className}__content`}>
-                      <a id={`title-button`}
-                      className={`title-button`}
-                        onClick={() => this._fetchUserLists.bind(userId, 5, 1)}>
-                        {userDisplayName}
-                      </a>
-                      {bookItems}
-                    </div>
-                  </div>
-                </div>,
-      errorInfo = this.state.errorInfo,
-      errorStatus,
-      errorTitle;
-
-    // Throw error message if something's wrong
-    if (errorInfo) {
-      errorStatus = errorInfo.status;
-      errorTitle = errorInfo.title;
-      content = <div className='bookItemList-wrapper'>
-                  <ErrorMessage errorClass='book-item-list-error'
-                    messageContent={this.props.errorMessage.failedRequest} />
-                </div>;
-      console.warn(`Server returned a ${errorStatus} status. ${errorTitle}.`);
-    }
-
-    // Render the list of owners on DOM
-    return (
-      <div id='main'>
-        <DocMeta tags={tags} />
-        <Hero name={listName} intro={listIntro}/>
-        {content}
-      </div>
-    );
   },
 
   /**
@@ -145,7 +48,7 @@ let BookItemList = React.createClass({
   *
   * @param (String) userId
   * @param (String) pageSize
-  *@ param (String) pageNumber
+  * @param (String) pageNumber
   */
   _fetchUserLists(userId, pageSize, pageNumber) {
     if (!userId || !pageSize || !pageNumber) {
@@ -160,13 +63,14 @@ let BookItemList = React.createClass({
       axios
         .get(`/browse/recommendations/lists/api/ajax/username/${userId}&${pageSize}&${pageNumber}`)
         .then(response => {
+          const list = response.data;
           // Update the store for the list of lists a user has.
-          Actions.updateUserLists(response.data.data);
-          Actions.updateListsNumber(response.data.listsNumber);
+          Actions.updateUserLists(list.data);
+          Actions.updateListsNumber(list.listsNumber);
           // Check if any error from the Refinery
-          if (response.data.errorInfo) {
-            Actions.failedData(response.data.errorInfo);
-            console.warn(`Server returned a ${response.data.errorInfo.status} status. ${response.data.errorInfo.title}.`);
+          if (list.errorInfo) {
+            Actions.failedData(list.errorInfo);
+            console.warn(`Server returned a ${list.errorInfo.status} status. ${list.errorInfo.title}.`);
           }
         })
         .then(response => {
@@ -175,13 +79,6 @@ let BookItemList = React.createClass({
     } else {
       this.context.router.push(`/browse/recommendations/lists/${userId}`);
     }
-  },
-
-  _transitionToUser(userId) {
-    // Now transit to the route.
-    this.transitionTo('UserLists', {
-      UserLists: userId
-    });
   },
 
   /**
@@ -194,21 +91,127 @@ let BookItemList = React.createClass({
   * Thus, this function is here for the app to fetch the data again.
   */
   _fetchBookData() {
-    let urlBookItemList = (window.location.pathname).split('/'),
-      urlBookItemListId = urlBookItemList[5];
+    const urlBookItemList = (window.location.pathname).split('/');
+    const urlBookItemListId = urlBookItemList[5];
 
-      axios
-        .get(`/browse/recommendations/lists/api/ajax/listID/${urlBookItemListId}`)
-        .then(response => {
-          // Update the Store for a specific list of books:
-          Actions.updateBookList(response.data.data);
-          // Check if any error from the Refinery
-          if (response.data.errorInfo) {
-            Actions.failedData(response.data.errorInfo);
-            console.warn(`Server returned a ${response.data.errorInfo.status} status. ${response.data.errorInfo.title}.`);
-          }
-        });
-  }
+    axios
+      .get(`/browse/recommendations/lists/api/ajax/listID/${urlBookItemListId}`)
+      .then(response => {
+        // Update the Store for a specific list of books:
+        Actions.updateBookList(response.data.data);
+        // Check if any error from the Refinery
+        if (response.data.errorInfo) {
+          Actions.failedData(response.data.errorInfo);
+          console.warn(`Server returned a ${response.data.errorInfo.status} status. ${response.data.errorInfo.title}.`);
+        }
+      });
+  },
+
+  // Render DOM
+  render() {
+    // The variable to store the data from Store
+    const bookItemList = this.state.bookItemList ? this.state.bookItemList : [];
+    const listId = bookItemList.id || '';
+    const listItems = bookItemList['list-items'] ? bookItemList['list-items'] : [];
+    const encoreUrl = 'http://browse.nypl.org/iii/encore/record/C__Rb';
+    const description = listIntro || "A list created by staff at The New York Public Library";
+    let userId = bookItemList.user ? bookItemList.user.id : '';
+    let userDisplayName = bookItemList.user ? bookItemList.user.attributes.name : '';
+    let listName = bookItemList.attributes ? bookItemList.attributes['list-name'] : '';
+    let listIntro = bookItemList.attributes ? bookItemList.attributes['list-description'] : '';
+    let pageTags = [
+      // Required OG meta tags
+      { property: 'og:title', content: `${listName} | The New York Public Library` },
+      { property: 'og:url', content: `http://www.nypl.org/browse/recommendations/lists/${userId}/${listId}` },
+      { property: 'og:image', content: '' },
+      { property: 'og:description', content: description },
+      { name: 'twitter:title', content: `${listName} | The New York Public Library` },
+      { name: 'twitter:description', content: description },
+      { name: 'twitter:image', content: '' }
+    ];
+    let tags = utils.metaTagUnion(pageTags);
+    let bookItems = (listItems && listItems.length) ?
+      listItems.map((element, i) => {
+        let target = `${encoreUrl}${element.item.id}?lang=eng`;
+        let itemId = element.item.id;
+        let bookItemName = element.item.attributes.title;
+        let bookItemDescription = element.attributes.annotation;
+        let bookCoverIsbn = element.item.attributes.isbns[0];
+        let authors = (element.item.attributes.authors.length) ?
+            `By ${element.item.attributes.authors}` : "The author of this item is not available";
+        let catalogInfo = `${element.item.attributes.format} - ${publishedDate}`;
+
+        const publishedDate = (element.item.attributes['publication-date'] && !undefined) ?
+          element.item.attributes['publication-date'] : "publish date is not available";
+        return (
+          <BookItem
+            id="bookItem"
+            className="bookItem"
+            key={i}
+            itemId={itemId}
+            target={target}
+            catalogInfo={catalogInfo}
+            bookItemName={bookItemName}
+            bookItemDescription={bookItemDescription}
+            bookCoverIsbn={bookCoverIsbn}
+            authors={authors} />
+        );
+      })
+      : <ErrorMessage
+        errorClass="book-item-list-error no-data"
+        messageContent={this.props.errorMessage.noData}
+        />;
+    let content = (
+      <div className="bookItemList-wrapper">
+        <BackButton
+          userId={userId}
+          userDisplayName={userDisplayName}
+        />
+        <div id={this.props.id} className={this.props.className}>
+          <div
+            id={`${this.props.id}__${listName}`}
+            className={`${this.props.className}__content`}>
+            <a
+              id="title-button"
+              className="title-button"
+              onClick={() => this._fetchUserLists.bind(userId, 5, 1)}>
+              {userDisplayName}
+            </a>
+            {bookItems}
+          </div>
+        </div>
+      </div>);
+    const errorInfo = this.state.errorInfo;
+    let errorStatus;
+    let errorTitle;
+
+    // Throw error message if something's wrong
+    if (errorInfo) {
+      errorStatus = errorInfo.status;
+      errorTitle = errorInfo.title;
+      content = (
+        <div className="bookItemList-wrapper">
+          <ErrorMessage
+            errorClass="book-item-list-error"
+            messageContent={this.props.errorMessage.failedRequest}
+          />
+        </div>
+        );
+      console.warn(`Server returned a ${errorStatus} status. ${errorTitle}.`);
+    }
+
+    // Render the list of owners on DOM
+    return (
+      <div id="main">
+        <DocMeta tags={tags} />
+        <Hero
+          name={listName}
+          intro={listIntro}
+        />
+        {content}
+      </div>
+    );
+  },
 });
 
 BookItemList.defaultProps = {
@@ -217,8 +220,8 @@ BookItemList.defaultProps = {
   className: 'bookItemList',
   errorMessage: {
     noData: 'No book in this list.',
-    failedRequest: 'Unable to complete this request. Something might be wrong with the server.'
-  }
+    failedRequest: 'Unable to complete this request. Something might be wrong with the server.',
+  },
 };
 
 export default BookItemList;
